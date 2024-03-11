00060579d55e98d22032cc7bd49135bcd51f514e
itsjackie00 <jackiedanso33@gmail.com>
2024-03-11 18:28:10 +0100

    added game results

--- js/script.js
+++ js/script.js
@@ -6,21 +6,38 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 */
 
+let esempio = 'mi chiamo jacqueline';
+//console.log(esempio);
 
-let userEmail = 'giovanni@gmail.com'.toLowerCase();
+
+
+
+//console.log('Element = ' + document.getElementById('userEmail'));
+//console.log('Value = ' + document.getElementById('userEmail').placeholder);
+//console.log(userEmail)
 let corretto = false;
-
+let elButton = document.querySelector('.btn.btn-success');
+//console.log(elButton);
 
 let text = ['giovanni@gmail.com', 'valeria234@gmail.com', 'patrizzia@hotmail.com', 'frascotti12@yahoo.com'];
-for (let i = 0; i < text.length; i++) {
-    if (userEmail === text[i]) {
-        corretto = true;
-        console.log('Benvenuto');
+
+elButton.addEventListener('click', function () {
+    let userEmail = (document.getElementById('userEmail'));
+    console.log(userEmail);
+
+    //console.log(userEmail.value);
+    for (let i = 0; i < text.length; i++) {
+        console.log(text[i],i);
+        if (userEmail.value === text[i]) {
+            corretto = true;
+            console.log(userEmail);
+            document.getElementById('result').innerHTML = 'Bevenuto';
+        }
     }
-}
-if (corretto === false) {
-    console.log('Non sei registrato');  
-}
+    if (corretto === false) {
+        document.getElementById('result').innerHTML = 'Non sei registrato';
+    }
+
 
 /*
 Gioco dei dadi
@@ -29,18 +46,30 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
 if (corretto === true) {
-    let computer =  getRndInteger(1,6); 
+    let computer = getRndInteger(1, 6);
     console.log(computer);
-    let user =  getRndInteger(1,6);
+    let user = getRndInteger(1, 6);
     console.log(user);
-    if (computer > user){
-        console.log('Computer is the winner');
-    }else if(computer === user){
-        console.log('Draw');
-    } else{
-        console.log('User is the winner');
+    if (computer > user) {
+        document.getElementById('message').innerHTML= `Computer is the winner !!! `;
+        document.getElementById('numbers').innerHTML= `Computer choosed ${computer} user choosed ${user}`;
+    } else if (computer === user) {
+        document.getElementById('message').innerHTML= `Is a draw`;
+        document.getElementById('numbers').innerHTML= `Computer choosed ${computer} user choosed ${user}`;
+
+    } else {
+        document.getElementById('message').innerHTML= `User is the winner  !!!`;
+        document.getElementById('numbers').innerHTML= `Computer choosed ${computer} user choosed ${user}`;
+
+
     }
 }
+
+}
+)
+
+
+
 
 
 
